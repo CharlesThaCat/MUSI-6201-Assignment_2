7e51e1f895ad9e9cc13db5d94673bfe5ca19d927
Charles Lin <bhxxlzw@126.com>
2019-09-27 11:19:28 -0400

    Section A finished

--- ass2solution.py
+++ ass2solution.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from scipy.io.wavfile import read
 import glob
 import os
+import math
 
 ########################### A. Feature Extraction ###########################
 # 1
@@ -13,8 +14,9 @@ def extract_spectral_centroid(xb, fs):
         length = len(b)
         b = np.multiply(b, compute_hann(length))
         magnitudes = np.abs(np.fft.rfft(b)) # magnitude spectrum for positive frequencies
-        freqs = np.abs(np.fft.rfftfreq(length, 1.0/fs) # positive frequencies 
+        freqs = np.abs(np.fft.rfftfreq(length, 1.0/fs)) # positive frequencies
         vsc = np.sum(magnitudes*freqs) / np.sum(magnitudes)
+        # vsc = np.sum(magnitudes*freqs) / np.sum(magnitudes)
         vsc_matrix.append(vsc)
     return np.array(vsc_matrix)
 
@@ -25,6 +27,7 @@ def extract_rms(xb):
         rms = np.sqrt(np.divide(np.sum(np.square(b)),length))
         rms = 20*np.log10(rms)
         rms_matrix.append(rms)
+    # print(rms_matrix)
     return np.array(rms_matrix)
 
 def extract_zerocrossingrate(xb):
@@ -81,22 +84,22 @@ def extract_features(x, blockSize, hopSize, fs):
     features[4,:] = extract_spectral_flux(xb)
     return features
 
-def block_audio(x, blockSize, hopSize, fs):
-    i = 0
-    xb = []
-    timeInSec = []
-    while i < len(x):
-        timeInSec.append(i / fs)
-        chunk = x[i: i + blockSize]
-        if len(chunk) != blockSize:
-            chunk = np.append(chunk, np.zeros(blockSize - len(chunk)))
-            xb.append(chunk)
-            break
-        else:
-            xb.append(chunk)
-        i += hopSize
+def block_audio(x,blockSize,hopSize,fs):
+    # allocate memory
+    numBlocks = math.ceil(x.size / hopSize)
+    xb = np.zeros([numBlocks, blockSize])
+    # compute time stamps
+    t = (np.arange(0, numBlocks) * hopSize) / fs
 
-    return [np.array(xb), np.array(timeInSec)]
+    x = np.concatenate((x, np.zeros(blockSize)),axis=0)
+
+    for n in range(0, numBlocks):
+        i_start = n * hopSize
+        i_stop = np.min([x.size - 1, i_start + blockSize - 1])
+
+        xb[n][np.arange(0,blockSize)] = x[np.arange(i_start, i_stop + 1)]
+
+    return (xb,t)
 
 
 # 3    
@@ -105,207 +108,229 @@ def aggregate_feature_per_file(features):
     for i in range(0,5):
         aggFeatures[2*i,:] = np.mean(features[i,:])
         aggFeatures[2*i+1,:] = np.std(features[i,:])
+    # print(aggFeatures)
     return aggFeatures
 
 
 # 4
 def get_feature_data(path, blockSize, hopSize):
-    
+    file_path = os.path.join(path, '*.wav')
+    wav_files = [f for f in glob.glob(file_path)]
+    num_of_files = np.array(wav_files).shape[0]
+    featureData = np.zeros((10,num_of_files))
+
+    k = 0
+    for wav_file in wav_files:
+        fs, audio = read(wav_file)
+        features = extract_features(audio, blockSize, hopSize, fs)
+        aggFeatures = aggregate_feature_per_file(features)
+        # featureData.append(aggFeatures)
+        # print(featureData[:,0:1])
+        featureData[:,k:k+1] = aggFeatures
+        k = k+1
 
     return featureData
 
-def get_f0_from_acf(r, fs):
-    peaks = find_peaks(r)[0]
-    # plt.plot(r)
-    # plt.plot(peaks, r[peaks], 'rs')
-    # plt.show()
-    if len(peaks) >= 2:
-        p = sorted(r[peaks])[::-1]
-        sorted_arg = np.argsort(r[peaks])[::-1]
-        f0 = fs / abs(peaks[sorted_arg][1] - peaks[sorted_arg][0])
-        return f0
-    return 0
+if __name__ == "__main__":
+    blockSize = 1024
+    hopSize = 256
+    path = 'C:/Users/bhxxl/Downloads/music_speech/music_speech/music_wav'
+    featureData = get_feature_data(path, blockSize, hopSize)
 
-# 4
-def track_pitch_acf(x, blockSize, hopSize, fs):
-    blocked_x, timeInSec = block_audio(x, blockSize, hopSize, fs)
-    frequencies = []
-    for b in blocked_x:
-        acf = comp_acf(b)
-        f0 = get_f0_from_acf(acf, fs)
-        frequencies.append(f0)
-    return [np.array(frequencies), timeInSec]
+    print(featureData.shape)
 
-########################### B. Evaluation ###########################
+# def get_f0_from_acf(r, fs):
+#     peaks = find_peaks(r)[0]
+#     # plt.plot(r)
+#     # plt.plot(peaks, r[peaks], 'rs')
+#     # plt.show()
+#     if len(peaks) >= 2:
+#         p = sorted(r[peaks])[::-1]
+#         sorted_arg = np.argsort(r[peaks])[::-1]
+#         f0 = fs / abs(peaks[sorted_arg][1] - peaks[sorted_arg][0])
+#         return f0
+#     return 0
 
-def gen_sin(f1=441, f2=882, fs=44100):
-    t1 = np.linspace(0, 1, fs)
-    t2 = np.linspace(1, 2, fs)
-    sin_441 = np.sin(2 * np.pi * 441 * t1)
-    sin_882 = np.sin(2 * np.pi * 882 * t2)
-    sin = np.append(sin_441, sin_882)
-    return sin
+# # 4
+# def track_pitch_acf(x, blockSize, hopSize, fs):
+#     blocked_x, timeInSec = block_audio(x, blockSize, hopSize, fs)
+#     frequencies = []
+#     for b in blocked_x:
+#         acf = comp_acf(b)
+#         f0 = get_f0_from_acf(acf, fs)
+#         frequencies.append(f0)
+#     return [np.array(frequencies), timeInSec]
 
-def code_for_B1():
-    fs = 44100
-    f1 = 441
-    f2 = 882
-    sin = gen_sin(f1, f2, fs)
-    [frequencies, timeInSec] = track_pitch_acf(sin, 1024, 512, fs)
-    error = np.zeros(len(timeInSec))
-    error[:len(timeInSec) // 2] += f1
-    error[len(timeInSec) // 2 :] += f2
-    error = np.abs(error - frequencies)
+# ########################### B. Evaluation ###########################
 
-    # Plot
-    line1, = plt.plot(timeInSec, error)
-    line2, = plt.plot(timeInSec, frequencies)
-    plt.legend((line1, line2), ("error", "frequencies (f0)"))
-    plt.title("Resulting f0 and error in Hz")
-    plt.xlabel("samples (sec)")
-    plt.ylabel("Frequency (Hz)")
-    plt.show()
+# def gen_sin(f1=441, f2=882, fs=44100):
+#     t1 = np.linspace(0, 1, fs)
+#     t2 = np.linspace(1, 2, fs)
+#     sin_441 = np.sin(2 * np.pi * 441 * t1)
+#     sin_882 = np.sin(2 * np.pi * 882 * t2)
+#     sin = np.append(sin_441, sin_882)
+#     return sin
 
-# 1
-code_for_B1()
+# def code_for_B1():
+#     fs = 44100
+#     f1 = 441
+#     f2 = 882
+#     sin = gen_sin(f1, f2, fs)
+#     [frequencies, timeInSec] = track_pitch_acf(sin, 1024, 512, fs)
+#     error = np.zeros(len(timeInSec))
+#     error[:len(timeInSec) // 2] += f1
+#     error[len(timeInSec) // 2 :] += f2
+#     error = np.abs(error - frequencies)
 
-def convert_freq2midi(freqInHz):
-    return 69 + 12 * np.log2(freqInHz / 440.0)
+#     # Plot
+#     line1, = plt.plot(timeInSec, error)
+#     line2, = plt.plot(timeInSec, frequencies)
+#     plt.legend((line1, line2), ("error", "frequencies (f0)"))
+#     plt.title("Resulting f0 and error in Hz")
+#     plt.xlabel("samples (sec)")
+#     plt.ylabel("Frequency (Hz)")
+#     plt.show()
 
-def freq2cent(freqInHz):
-    return 1200 * np.log2(freqInHz / 440.0)
+# # 1
+# code_for_B1()
 
-def eval_pitchtrack(estimateInHz, groundtruthInHz):
-    centError = []
-    for i in range(len(groundtruthInHz)):
-        if groundtruthInHz[i] != 0:
-            if estimateInHz[i] != 0:
-                centError.append(freq2cent(estimateInHz[i]) - freq2cent(groundtruthInHz[i]))
-            elif estimateInHz[i] == 0:
-                centError.append(-freq2cent(groundtruthInHz[i]))
-    centError = np.array(centError)
-    rms = np.sqrt(np.mean(np.square(centError)))
-    return rms
+# def convert_freq2midi(freqInHz):
+#     return 69 + 12 * np.log2(freqInHz / 440.0)
 
-def run_evaluation(complete_path_to_data_folder):
-    file_path = os.path.join(complete_path_to_data_folder, '*.wav')
-    wav_files = [f for f in glob.glob(file_path)]
-    errCentRms = []
-    for wav_file in wav_files:
-        name = os.path.split(wav_file)[1].split('.')[0]
-        txt_file = os.path.join(complete_path_to_data_folder, name+'.f0.Corrected.txt')
-        with open(txt_file) as f:
-            annotations = f.readlines()
-        for i in range(len(annotations)):
-            annotations[i] = list(map(float, annotations[i][:-2].split('     ')))
-        annotations = np.array(annotations)
-        fs, audio = read(wav_file)
-        freq, timeInSec = track_pitch_acf(audio, 2048, 512, fs)
-        trimmed_freq = np.ones(freq.shape)
-        trimmed_annotations = np.ones(freq.shape)
-        for i in range(len(freq)):
-            if annotations[i, 2] > 0:
-                trimmed_freq[i] = freq[i]
-                trimmed_annotations[i] = annotations[i, 2]
-        # plt.plot(trimmed_freq)
-        # plt.plot(trimmed_annotations)
-        # plt.show()
-        errCentRms.append(eval_pitchtrack(trimmed_freq, trimmed_annotations))
-    errCentRms = np.array(errCentRms)
-    # print(errCentRms)
-    return np.mean(errCentRms)
+# def freq2cent(freqInHz):
+#     return 1200 * np.log2(freqInHz / 440.0)
 
-print("Overall errCentRms:",run_evaluation("trainData/"))
+# def eval_pitchtrack(estimateInHz, groundtruthInHz):
+#     centError = []
+#     for i in range(len(groundtruthInHz)):
+#         if groundtruthInHz[i] != 0:
+#             if estimateInHz[i] != 0:
+#                 centError.append(freq2cent(estimateInHz[i]) - freq2cent(groundtruthInHz[i]))
+#             elif estimateInHz[i] == 0:
+#                 centError.append(-freq2cent(groundtruthInHz[i]))
+#     centError = np.array(centError)
+#     rms = np.sqrt(np.mean(np.square(centError)))
+#     return rms
 
-########################### C. Bonus ###########################
+# def run_evaluation(complete_path_to_data_folder):
+#     file_path = os.path.join(complete_path_to_data_folder, '*.wav')
+#     wav_files = [f for f in glob.glob(file_path)]
+#     errCentRms = []
+#     for wav_file in wav_files:
+#         name = os.path.split(wav_file)[1].split('.')[0]
+#         txt_file = os.path.join(complete_path_to_data_folder, name+'.f0.Corrected.txt')
+#         with open(txt_file) as f:
+#             annotations = f.readlines()
+#         for i in range(len(annotations)):
+#             annotations[i] = list(map(float, annotations[i][:-2].split('     ')))
+#         annotations = np.array(annotations)
+#         fs, audio = read(wav_file)
+#         freq, timeInSec = track_pitch_acf(audio, 2048, 512, fs)
+#         trimmed_freq = np.ones(freq.shape)
+#         trimmed_annotations = np.ones(freq.shape)
+#         for i in range(len(freq)):
+#             if annotations[i, 2] > 0:
+#                 trimmed_freq[i] = freq[i]
+#                 trimmed_annotations[i] = annotations[i, 2]
+#         # plt.plot(trimmed_freq)
+#         # plt.plot(trimmed_annotations)
+#         # plt.show()
+#         errCentRms.append(eval_pitchtrack(trimmed_freq, trimmed_annotations))
+#     errCentRms = np.array(errCentRms)
+#     # print(errCentRms)
+#     return np.mean(errCentRms)
 
-def block_audio_mod(x, blockSize, hopSize, fs):
-    i = 0
-    xb = []
-    timeInSec = []
-    while i < len(x):
-        timeInSec.append(i / fs)
-        chunk = x[i: i + blockSize]
-        if len(chunk) != blockSize:
-            chunk = np.append(chunk, np.zeros(blockSize - len(chunk)))
-            xb.append(chunk*np.hamming(blockSize))
-            break
-        else:
-            xb.append(chunk*np.hamming(blockSize))
-        i += hopSize
+# print("Overall errCentRms:",run_evaluation("trainData/"))
 
-    return [np.array(xb), np.array(timeInSec)]
+# ########################### C. Bonus ###########################
 
-def get_f0_from_acfmod(r, fs):
-    peaks = find_peaks(r, height=0, distance=50)[0]
-    if len(peaks) >= 2:
-        sorted_arg = np.argsort(r[peaks])[::-1]
-        px, py = parabolic(r,peaks[sorted_arg][0])
-        return fs/px
-    return 0
+# def block_audio_mod(x, blockSize, hopSize, fs):
+#     i = 0
+#     xb = []
+#     timeInSec = []
+#     while i < len(x):
+#         timeInSec.append(i / fs)
+#         chunk = x[i: i + blockSize]
+#         if len(chunk) != blockSize:
+#             chunk = np.append(chunk, np.zeros(blockSize - len(chunk)))
+#             xb.append(chunk*np.hamming(blockSize))
+#             break
+#         else:
+#             xb.append(chunk*np.hamming(blockSize))
+#         i += hopSize
 
-def track_pitch_acfmod(x, blockSize, hopSize, fs):
-    blocked_x, timeInSec = block_audio_mod(x, blockSize, hopSize, fs)
-    frequencies = []
-    for b in blocked_x:
-        acf = comp_acf(b)
-        f0 = get_f0_from_acfmod(acf, fs)
-        frequencies.append(f0)
-    frequencies = np.array(frequencies)
-    frequencies = medfilt(frequencies,kernel_size=9)
-    return [frequencies, timeInSec]
+#     return [np.array(xb), np.array(timeInSec)]
 
-def parabolic(f, x):
-    """Quadratic interpolation for estimating the true position of an
-    inter-sample maximum when nearby samples are known.
-    f is a vector and x is an index for that vector.
-    Returns (vx, vy), the coordinates of the vertex of a parabola that goes
-    through point x and its two neighbors.
-    """
-    xv = 1/2. * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
-    yv = f[x] - 1/4. * (f[x-1] - f[x+1]) * (xv - x)
-    return (xv, yv)
+# def get_f0_from_acfmod(r, fs):
+#     peaks = find_peaks(r, height=0, distance=50)[0]
+#     if len(peaks) >= 2:
+#         sorted_arg = np.argsort(r[peaks])[::-1]
+#         px, py = parabolic(r,peaks[sorted_arg][0])
+#         return fs/px
+#     return 0
 
-# fs = 44100
-# f1 = 441
-# f2 = 882
-# sin = gen_sin(f1, f2, fs)
-# [frequencies, timeInSec] = track_pitch_acfmod(sin, 441, 441, fs)
-# error = np.zeros(len(timeInSec))
-# error[:len(timeInSec) // 2] += f1
-# error[len(timeInSec) // 2 :] += f2
-# error = np.abs(error - frequencies)
-# plt.plot(timeInSec, error)
-# plt.plot(timeInSec, frequencies)
-# plt.show()
+# def track_pitch_acfmod(x, blockSize, hopSize, fs):
+#     blocked_x, timeInSec = block_audio_mod(x, blockSize, hopSize, fs)
+#     frequencies = []
+#     for b in blocked_x:
+#         acf = comp_acf(b)
+#         f0 = get_f0_from_acfmod(acf, fs)
+#         frequencies.append(f0)
+#     frequencies = np.array(frequencies)
+#     frequencies = medfilt(frequencies,kernel_size=9)
+#     return [frequencies, timeInSec]
 
-def run_evaluation_mod(complete_path_to_data_folder):
-    file_path = os.path.join(complete_path_to_data_folder, '*.wav')
-    wav_files = [f for f in glob.glob(file_path)]
-    errCentRms = []
-    for wav_file in wav_files:
-        name = os.path.split(wav_file)[1].split('.')[0]
-        txt_file = os.path.join(complete_path_to_data_folder, name+'.f0.Corrected.txt')
-        with open(txt_file) as f:
-            annotations = f.readlines()
-        for i in range(len(annotations)):
-            annotations[i] = list(map(float, annotations[i][:-2].split('     ')))
-        annotations = np.array(annotations)
-        fs, audio = read(wav_file)
-        freq, timeInSec = track_pitch_acfmod(audio, 2048, 512, fs)
-        trimmed_freq = np.ones(freq.shape)
-        trimmed_annotations = np.ones(freq.shape)
-        for i in range(len(freq)):
-            if annotations[i, 2] > 0:
-                trimmed_freq[i] = freq[i]
-                trimmed_annotations[i] = annotations[i, 2]
-        plt.plot(trimmed_freq, label='frequency (Hz)')
-        plt.plot(trimmed_annotations, label='annotation')
-        plt.legend()
-        plt.show()
-        errCentRms.append(eval_pitchtrack(trimmed_freq, trimmed_annotations))
-    errCentRms = np.array(errCentRms)
-    # print(errCentRms)
-    return np.mean(errCentRms)
+# def parabolic(f, x):
+#     """Quadratic interpolation for estimating the true position of an
+#     inter-sample maximum when nearby samples are known.
+#     f is a vector and x is an index for that vector.
+#     Returns (vx, vy), the coordinates of the vertex of a parabola that goes
+#     through point x and its two neighbors.
+#     """
+#     xv = 1/2. * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
+#     yv = f[x] - 1/4. * (f[x-1] - f[x+1]) * (xv - x)
+#     return (xv, yv)
 
-print("Overall errCentRms (mod):",run_evaluation_mod("trainData/"))
+# # fs = 44100
+# # f1 = 441
+# # f2 = 882
+# # sin = gen_sin(f1, f2, fs)
+# # [frequencies, timeInSec] = track_pitch_acfmod(sin, 441, 441, fs)
+# # error = np.zeros(len(timeInSec))
+# # error[:len(timeInSec) // 2] += f1
+# # error[len(timeInSec) // 2 :] += f2
+# # error = np.abs(error - frequencies)
+# # plt.plot(timeInSec, error)
+# # plt.plot(timeInSec, frequencies)
+# # plt.show()
+
+# def run_evaluation_mod(complete_path_to_data_folder):
+#     file_path = os.path.join(complete_path_to_data_folder, '*.wav')
+#     wav_files = [f for f in glob.glob(file_path)]
+#     errCentRms = []
+#     for wav_file in wav_files:
+#         name = os.path.split(wav_file)[1].split('.')[0]
+#         txt_file = os.path.join(complete_path_to_data_folder, name+'.f0.Corrected.txt')
+#         with open(txt_file) as f:
+#             annotations = f.readlines()
+#         for i in range(len(annotations)):
+#             annotations[i] = list(map(float, annotations[i][:-2].split('     ')))
+#         annotations = np.array(annotations)
+#         fs, audio = read(wav_file)
+#         freq, timeInSec = track_pitch_acfmod(audio, 2048, 512, fs)
+#         trimmed_freq = np.ones(freq.shape)
+#         trimmed_annotations = np.ones(freq.shape)
+#         for i in range(len(freq)):
+#             if annotations[i, 2] > 0:
+#                 trimmed_freq[i] = freq[i]
+#                 trimmed_annotations[i] = annotations[i, 2]
+#         plt.plot(trimmed_freq, label='frequency (Hz)')
+#         plt.plot(trimmed_annotations, label='annotation')
+#         plt.legend()
+#         plt.show()
+#         errCentRms.append(eval_pitchtrack(trimmed_freq, trimmed_annotations))
+#     errCentRms = np.array(errCentRms)
+#     # print(errCentRms)
+#     return np.mean(errCentRms)
+
+# print("Overall errCentRms (mod):",run_evaluation_mod("trainData/"))
